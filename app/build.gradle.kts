aceec2e07b3ef3142fa461f757af644bb6d49d2e
Farhat <f190886@mail.ru>
2024-05-13 21:59:40 +0400

    project pogoda

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,6 +50,8 @@ android {
 }
 
 dependencies {
+    implementation("com.google.accompanist:accompanist-pager:0.19.0")
+    implementation("com.google.accompanist:accompanist-pager-indicators:0.19.0")
 
     implementation("androidx.core:core-ktx:1.13.0")
     implementation("com.android.volley:volley:1.2.1")
@@ -60,6 +62,8 @@ dependencies {
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
+    implementation("io.coil-kt:coil-compose:2.0.0-rc01")
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- app/src/main/java/com/example/pogoda/MainActivity.kt
+++ app/src/main/java/com/example/pogoda/MainActivity.kt
@@ -3,8 +3,10 @@ package com.example.pogoda
 import android.app.DownloadManager.Request
 import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -22,93 +24,126 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
+import com.example.pogoda.data.WeatherModel
 import com.example.pogoda.ui.theme.PogodaTheme
+import com.example.pogoda.ui.theme.screens.MainCard
+import com.example.pogoda.ui.theme.screens.TabLayout
 import org.json.JSONObject
 
 
-const val API_KEY = "e5263f3dc7184af3ac6193051242404"
+const val API_KEY = "" // вставить API KEY после регистрации на сайте weatherapi.com
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             PogodaTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("London", this)
+                val daysList = remember {
+                    mutableStateOf(listOf<WeatherModel>())
                 }
+                val dialogState = remember {
+                    mutableStateOf(false)
+                }
+
+                val currentDay = remember {
+                    mutableStateOf(WeatherModel(
+                        "",
+                        "",
+                        "0.0",
+                        "",
+                        "",
+                        "0.0",
+                        "0.0",
+                        ""
+                    ))
+                }
+                if (dialogState.value){
+                    DialogSearch(dialogState, onSubmit = {
+                        getData(it, this, daysList, currentDay)
+                    })
+                }
+                getData("Ulyanovsk", this, daysList, currentDay)
+                Image(
+                    painter = painterResource(id = R.drawable.weather2),
+                    contentDescription = "im1",
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .alpha(0.6f),
+                    contentScale = ContentScale.FillBounds
+                )
+                Column {
+                    MainCard(currentDay, onClickSync = {
+                        getData("Moscow", this@MainActivity, daysList, currentDay)
+                    },
+                        onClickSearch = {
+                           dialogState.value = true
+                        })
+                    TabLayout(daysList, currentDay)
+                }
+
             }
         }
     }
 }
 
-@Composable
-fun Greeting(sity: String, context: Context) {
-    val state = remember{
-        mutableStateOf("")}
-    val status = remember{
-        mutableStateOf("")
-    }
-    Column(modifier = Modifier.fillMaxSize()) {
-        Box(modifier = Modifier
-            .fillMaxHeight(0.3f)
-            .fillMaxWidth(),
-            contentAlignment = Alignment.Center) {
-            Text(text = "Температура в городе $sity")
-        }
-        Box(modifier = Modifier
-            .fillMaxHeight(0.3f)
-            .fillMaxWidth(),
-            contentAlignment = Alignment.Center) {
-            Text(text = "${state.value} градусов по C")
-        }
-        Box(modifier = Modifier
-            .fillMaxHeight(0.3f)
-            .fillMaxWidth(),
-            contentAlignment = Alignment.Center) {
-            Text(text = "Дата и время сейчас ${status.value}")
-        }
-
-        Box(modifier = Modifier
-            .fillMaxHeight()
-            .fillMaxWidth()
-            .background(color = Color.Transparent),
-            contentAlignment = Alignment.BottomCenter) {
-
-            Button(onClick = {
-                getResault(sity, state, status, context)
-            }, modifier = Modifier
-                .padding(5.dp)
-                .fillMaxWidth()) {
-                Text(text = "Нажми для получения данных")
-            }
-
-        }
-    }
-
-}
-
-private fun getResault(city:String, state: MutableState<String>, status: MutableState<String>, context: Context) {
-    val url = "https://api.weatherapi.com/v1/current.json" + "?key=$API_KEY&" +
-            "q=$city" + "&aqi=no"
+private fun getData(city: String, context: Context, daysList: MutableState<List<WeatherModel>>,
+                    currentDay: MutableState<WeatherModel>){
+    val url = "https://api.weatherapi.com/v1/forecast.json?key=$API_KEY" +
+            "&q=$city" +
+            "&days=" +
+            "3" +
+            "&aqi=no&alerts=no"
     val queue = Volley.newRequestQueue(context)
-    val stringRequest = StringRequest(com.android.volley.Request.Method.GET, url,
-        { response ->
-            val obj = JSONObject(response)
-        state.value = obj.getJSONObject("current").getString("temp_c")
-            val tim = JSONObject(response)
-        status.value = tim.getJSONObject("location").getString("localtime")
+    val sRequest = StringRequest(
+        com.android.volley.Request.Method.GET,
+        url,
+        {
+            response ->
+            val list = getWeatherByDays(response)
+            currentDay.value = list[0]
+            daysList.value = list
         },
-        { error ->
-
+        {
+            Log.d("MyLog", "VolleyError: $it")
         }
     )
-    queue.add(stringRequest)
+    queue.add(sRequest)
+}
+
+private fun getWeatherByDays(response: String): List<WeatherModel>{
+    if (response.isEmpty()) return listOf()
+    val list = ArrayList<WeatherModel>()
+    val mainObject = JSONObject(response)
+    val city = mainObject.getJSONObject("location").getString("name")
+    val days = mainObject.getJSONObject("forecast").getJSONArray("forecastday")
+    for (i in 0 until days.length()){
+        val item = days[i] as JSONObject
+        list.add(
+            WeatherModel(
+                city,
+                item.getString("date"),
+                "",
+                item.getJSONObject("day").getJSONObject("condition").getString("text"),
+                item.getJSONObject("day").getJSONObject("condition").getString("icon"),
+                item.getJSONObject("day").getString("maxtemp_c"),
+                item.getJSONObject("day").getString("mintemp_c"),
+                item.getJSONArray("hour").toString()
+
+            )
+        )
+    }
+    list[0] = list[0].copy(
+        time = mainObject.getJSONObject("current").getString("last_updated"),
+        currentTemp = mainObject.getJSONObject("current").getString("temp_c"),
+
+    )
+    return list
 }
